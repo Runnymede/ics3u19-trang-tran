f2e7fa098f4f7c1cbcff8898b841f3d53e32adcf
333771657 <333771657@C-9R62YR1.tdsb.on.ca>
2019-11-26 15:05:54 -0500

    Update-Nov. 26, 2019

--- ICS3U-Trang/src/tran/unit3/GuesingGame.java
+++ ICS3U-Trang/src/tran/unit3/GuesingGame.java
@@ -16,52 +16,65 @@ public class GuesingGame {
 	 */
 	public static void main(String[] args) throws InterruptedException {
 		Scanner sc=new Scanner(System.in);
-		
+
 		//variables
 		String[] colour= {"Red","Orange","Yellow","Green","Blue","Indigo","Violet","Purple",
 				"White","Black","Gray","Brown","Cyan","Maroon","Gold","Baby blue","Lilac",
 				"Mahogany","Hot pink","Scarlet"};
 		int randomIndex;
 		String userAnswer;
-		
+		int guess=0;
+
 		//Adlib 
 		System.out.println("Hi! let's play a guessing game!\n"
-				+ "Please pick a colour from below and press a key when you're ready\n");
+				+ "Please pick a colour from below and wait for the game to start\n");
 		System.out.println("Red, Orange, Yellow, Green,\n"
 				+ "Blue, Indigo, Violet, Purple,\n"
 				+ "Black, Grey, Brown, Cyan, \n"
 				+ "Gold, Baby blue, Lilac, Mahogany,\n"
 				+ "Hot pink, Scarlet, White, Maroon");
 		
-		Thread.sleep(2000);
+		//2 second delay before the game
+		Thread.sleep(5000);
 		System.out.println();
-		
+
 		//Generates a guess and asks the user
 		randomIndex=(int)(Math.random()*20);
 		System.out.println("Is it "+colour[randomIndex]+"?");
 		userAnswer=sc.next();
-		
+
+		//rules the first answer out
+		colour[randomIndex]="nothing";
+
 		//Generates another guess until the user says "yes"
+		//Gives up if everything has been tried
 		while(userAnswer.equalsIgnoreCase("no"))
 		{
 			randomIndex=(int)(Math.random()*20);
-			
+
 			//picks another guess if it has asked that already
 			while(colour[randomIndex].equals("nothing"))
 			{
 				randomIndex=(int)(Math.random()*20);
 			}
-			
+
 			//asks the user to guess
 			System.out.println("Is it "+colour[randomIndex]+"?");
 			userAnswer=sc.next();
-			
+
 			//rules the guess (won't ask again)
 			colour[randomIndex]="nothing";
+			guess++;
+			
+			//print the "I give up" sequence when the computer has no more guesses
+			if (guess==20)
+			{
+				System.out.println("I give up!");
+			}
 		}
 		System.out.println("I guessed it!!");
 		
-		
+
 	}
 
 }
--- ICS3U-Trang/src/tran/unit3/TenThousandRolls.java
+++ ICS3U-Trang/src/tran/unit3/TenThousandRolls.java
@@ -14,25 +14,75 @@ public class TenThousandRolls {
 		//variables
 		int dice1;
 		int dice2;
-		int [] totalRolls=new int[10000];
+		int [] totalRolls=new int[11];
 		int total;
+		int counter=2;
 		
 		for (int i=1;i<=10000;i++)
 		{
 			dice1=(int)(Math.random()*6)+1;
 			dice2=(int)(Math.random()*6)+1;
-			
+
 			total=dice1+dice2;
-			
-			for (int j=0;j<10000;i++)
+
+			if (total==2)
 			{
-				totalRolls[j]=total;
+				totalRolls[0]+=1;
+			}
+			else if(total==3)
+			{
+				totalRolls[1]+=1;
+			}
+			else if(total==4)
+			{
+				totalRolls[2]+=1;
+			}
+			else if(total==5)
+			{
+				totalRolls[3]+=1;
+			}
+			else if(total==6)
+			{
+				totalRolls[4]+=1;
+			}
+			else if(total==7)
+			{
+				totalRolls[5]+=1;
+			}
+			else if(total==8)
+			{
+				totalRolls[6]+=1;
+			}
+			else if(total==9)
+			{
+				totalRolls[7]+=1;
+			}
+			else if(total==10)
+			{
+				totalRolls[8]+=1;
+			}
+			else if(total==11)
+			{
+				totalRolls[9]+=1;
+			}
+			else
+			{
+				totalRolls[10]+=1;
 			}
 			
 		}
 		
+		System.out.printf("%6s %20s", "Total", "Number of Rolls");
+		System.out.println();
 		
-		
-		
+		for(int j=0;j<11;j++) 
+		{
+			System.out.printf("%6s %20s", counter, totalRolls[j]);
+			System.out.println();
+			counter++;
+		}
+
+
+
 	}
 }
