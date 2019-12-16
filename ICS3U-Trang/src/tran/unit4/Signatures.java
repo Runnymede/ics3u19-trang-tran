dfd1b6ffa29798d28723f8fcc5fcd12041174a64
333771657 <333771657@C-9R62YR1.tdsb.on.ca>
2019-12-16 13:43:08 -0500

    December 16, 2019

---
+++ ICS3U-Trang/src/tran/unit4/Signatures.java
@@ -0,0 +1,31 @@
+package tran.unit4;
+/**
+ * Testing two methods that contain images.
+ * Date: December 16, 2019
+ * @author 333771657
+ */
+public class Signatures {
+	/**
+	 * Start of the program
+	 */
+	public static void printSignature() {
+		System.out.println(".-,.-.,-.    /`-.     /`-.    )\\  )\\     )\\.-.   ");
+		System.out.println(") ,, ,. (  ,' _  \\  ,' _  \\  (  \\, /   ,' ,-,_) ");
+		System.out.println("\\( |(  )/ (  '-' ( (  '-' (   ) \\ (   (  .   __");
+		System.out.println("   ) \\     ) ,_ .'  )   _  ) ( ( \\ \\   ) '._\\ _)");
+		System.out.println("   \\ (    (  ' ) \\ (  ,' ) \\  `.)/  ) (  ,   (   ");
+		System.out.println("    )/     )/   )/  )/    )/     '.(   )/'._.'   ");
+	}
+	public static void printImage() {
+		
+		System.out.println("\n >(.)__ <(.)__ =(.)__\r\n" + 
+				"  (___/  (___/  (___/");
+	}
+
+	public static void main(String[] args) {
+		
+		printSignature();
+		printImage();
+	}
+
+}
