e011fcd2a78a55dc6aeabefdc58b5e974c393933
Trang-TDSB <trang.tran2@student.tdsb.on.ca>
2019-12-17 20:53:43 -0500

    Last Update

--- ICS3U-Trang/src/tran/unit4/IsoTriangles.java
+++ ICS3U-Trang/src/tran/unit4/IsoTriangles.java
@@ -2,29 +2,58 @@ package tran.unit4;
 
 import java.util.Scanner;
 /**
- * Prompts the user for the number of lines that will make up the triangle.<br>
- * Displays the triangle using stars and spaces.<br><p>
+ * Prompts the user for the size of the triangle (the number of rows used)<br>
+ * Then, displays the triangle with stars.<br>
+ * This triangle uses spaces to neatly formatted to actually look like a triangle.<p>
+ * Example:<br>
+ * Enter the size:<br>
+ * 4<br>
+ *     *<br>
+ *    ***<br>
+ *   *****<br>
+ *  *******<br>
+ * <p>
  * Date: December 17, 2019
  * @author 333771657
  */
 public class IsoTriangles {
 
-	public static void drawSpaces(int spaces){
-		System.out.println(" ");
+	//Method "drawSpaces" decides how many spaces will be printed before the star
+	public static void drawSpaces(int amountOfSpaces){
+		for (int i=1;i<=(amountOfSpaces-1);i++)
+		{
+		System.out.print(" ");
+		}
 	}
-
+	
+	//Method "drawStars" decides how many stars will be printed to the screen
 	public static void drawStars(int stars) {
-		System.out.println("*");
+		for (int i=1;i<=stars;i++) {
+			System.out.print("*");
+		}
 	}
-
+	
+	//Main method 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-
-		System.out.println("Enter the size");
+		int rowCounter=1;
+		
+		//Gets user Input 
+		System.out.println("Enter the size:");
 		int sizeOfTriangle=sc.nextInt();
-
-		while () {
-			drawStars(sizeOfTriangle+(sizeOfTriangle-1));
+		
+		//calculates the initial value of the number of stars and spaces
+		int spaceCalculator=sizeOfTriangle+(sizeOfTriangle-2);
+		int starValue=1;
+		
+		//
+		while (rowCounter<=sizeOfTriangle) {
+			drawSpaces(spaceCalculator);
+			drawStars(starValue);
+			System.out.println();
+			rowCounter++;
+			spaceCalculator--;
+			starValue+=2;
 		}
 		
 	}
