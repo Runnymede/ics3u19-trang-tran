2a64f907779bed66ff1003f50d0f07fb363e57ea
Trang-TDSB <trang.tran2@student.tdsb.on.ca>
2020-01-03 18:11:05 -0500

    January 3, 2020

---
+++ ICS3U-Trang/src/extraChallenges/J1CCC.java
@@ -0,0 +1,43 @@
+package extraChallenges;
+
+import java.util.Scanner;
+
+public class J1CCC {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		int [] score=new int [6];
+		int s=3;
+		int totalBanana=0;
+		int totalApple=0;
+		
+		for (int i=0;i<score.length;i++) {
+			score[i]=sc.nextInt();
+		}
+		
+		for (int a=0;a<3;a++) {
+			totalApple+=score[a]*s;
+			s-=1;
+		}
+		
+		s=3;
+		for (int b=3;b<score.length;b++) {
+			totalBanana+=score[b]*s;
+			s-=1;
+		}
+		
+		if (totalApple<totalBanana) {
+			System.out.println("B");
+		}
+		else if (totalApple>totalBanana) {
+			System.out.println("A");
+		}
+		else {
+			System.out.println("T");
+		}
+			
+		
+	}
+
+}
---
+++ ICS3U-Trang/src/extraChallenges/J2CCC.java
@@ -0,0 +1,35 @@
+package extraChallenges;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class J2CCC {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		
+		int lines = Integer.valueOf(sc.nextLine());
+		String [] inputs = new String [lines];
+		
+		for (int i=0; i<lines; i++) {
+            inputs[i]=sc.nextLine();
+        }
+		
+		for (int i=0; i<lines; i++) {
+			String[] now=inputs[i].split(" ");
+			List<String> part = new ArrayList<>();
+			part.addAll(Arrays.asList(now));
+			
+			for (int a=1;a<=Integer.valueOf(part.get(0));a++) {
+				System.out.print(part.get(1));
+			}
+			System.out.println();
+		}
+		
+		
+
+	}
+
+}
