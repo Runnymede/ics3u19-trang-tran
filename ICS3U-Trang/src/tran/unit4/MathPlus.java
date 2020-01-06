e2fd6a90d469b8c7793b073779ec809e945721ff
Trang-TDSB <trang.tran2@student.tdsb.on.ca>
2020-01-06 11:56:10 -0500

    January 6,2020

--- ICS3U-Trang/src/tran/unit4/MathPlus.java
+++ ICS3U-Trang/src/tran/unit4/MathPlus.java
@@ -15,7 +15,7 @@ public class MathPlus {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		
-		int []originalArray= {99,1,5,99,103,1,103,9,107,1,13,107,111,2,111,10,115,1,115,5};
+		int []originalArray= {99,1,5,99,103,1,103,9,107,1,-13,107,111,2,111,10,115,1,115555,5};
 		
 		System.out.println("Minimum Value: "+min(originalArray));
 		System.out.println("Maximum Value: "+max(originalArray));
@@ -29,14 +29,19 @@ public class MathPlus {
 	 * @return The index of the smallest value
 	 */
 	
-	//TODO: Figure out how to find the index 
 	public static int min(int[]minArray) {
 		int currentMin=minArray[0];
-		
-		for (int i=1;i<minArray.length;i++) {
+		int index=0;
+		for (int i=0;i<minArray.length;i++) {
 			currentMin=Math.min(currentMin, minArray[i]);
 		}
-		return currentMin;
+		for (int i=0;i<minArray.length;i++) {
+			if (currentMin==minArray[i]) {
+				index=i;
+				break;
+			}
+		}
+		return index;
 	}
 	
 	/**
@@ -44,14 +49,20 @@ public class MathPlus {
 	 * @param maxArray receives an array
 	 * @return The index of the largest value
 	 */
-	//TODO: Figure out how to find the index 
 	public static int max(int[]maxArray) {
 		int currentMax=maxArray[0];
-		
-		for (int i=1;i<maxArray.length;i++) {
+		int index=0;
+		for (int i=0;i<maxArray.length;i++) {
 			currentMax=Math.max(currentMax, maxArray[i]);
 		}
-		return currentMax;
+		
+		for (int i=0;i<maxArray.length;i++) {
+			if (currentMax==maxArray[i]) {
+				index=i;
+				break;
+			}
+		}
+		return index;
 	}
 	
 	/**
