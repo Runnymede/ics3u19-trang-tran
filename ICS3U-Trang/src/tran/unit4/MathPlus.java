ff3fd493a74321cb4a3b0a73293f47b9dcb81d51
333771657 <333771657@C-9R62YR1.tdsb.on.ca>
2020-01-06 15:05:53 -0500

    part 1

--- ICS3U-Trang/src/tran/unit4/MathPlus.java
+++ ICS3U-Trang/src/tran/unit4/MathPlus.java
@@ -14,82 +14,147 @@ public class MathPlus {
 	 */
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
+
+		int []array= {99,1,5,99,103,1,103,9,107,1,-13,107,111,2,111,10,115,1,115555,5};
+		double[]array2= {2.45,3.90,4.15,5.23,6.56,1.36,7.93,8.35,9.75,10.0,1.36};
+
+		System.out.println("Minimum Value: "+min(array));
+		System.out.println("Maximum Value: "+max(array));
+		System.out.println("Sum: "+sum(array));
+		System.out.println("Average: "+average(array));
 		
-		int []originalArray= {99,1,5,99,103,1,103,9,107,1,-13,107,111,2,111,10,115,1,115555,5};
-		
-		System.out.println("Minimum Value: "+min(originalArray));
-		System.out.println("Maximum Value: "+max(originalArray));
-		System.out.println("Sum: "+sum(originalArray));
-		System.out.println("Average: "+average(originalArray));
+		System.out.println("\nMinimum Value: "+min(array2));
+		System.out.println("Maximum Value: "+max(array2));
+		System.out.println("Sum: "+sum(array2));
+		System.out.println("Average: "+average(array2));
 	}
 
 	/**
 	 * Method "min" receives an array and finds the index of the smallest value
-	 * @param minArray receives an array
+	 * @param numbers receives an array
 	 * @return The index of the smallest value
 	 */
-	
-	public static int min(int[]minArray) {
-		int currentMin=minArray[0];
+	public static int min(int[]numbers) {
+		int currentMin=numbers[0];
 		int index=0;
-		for (int i=0;i<minArray.length;i++) {
-			currentMin=Math.min(currentMin, minArray[i]);
+		for (int i=0;i<numbers.length;i++) {
+			currentMin=Math.min(currentMin, numbers[i]);
 		}
-		for (int i=0;i<minArray.length;i++) {
-			if (currentMin==minArray[i]) {
+		for (int i=0;i<numbers.length;i++) {
+			if (currentMin==numbers[i]) {
 				index=i;
 				break;
 			}
 		}
 		return index;
 	}
-	
+
 	/**
 	 * Method "max" receives an array and finds the index of the largest value
-	 * @param maxArray receives an array
+	 * @param numbers receives an array
 	 * @return The index of the largest value
 	 */
-	public static int max(int[]maxArray) {
-		int currentMax=maxArray[0];
+	public static int max(int[]numbers) {
+		int currentMax=numbers[0];
 		int index=0;
-		for (int i=0;i<maxArray.length;i++) {
-			currentMax=Math.max(currentMax, maxArray[i]);
+		for (int i=0;i<numbers.length;i++) {
+			currentMax=Math.max(currentMax, numbers[i]);
 		}
-		
-		for (int i=0;i<maxArray.length;i++) {
-			if (currentMax==maxArray[i]) {
+
+		for (int i=0;i<numbers.length;i++) {
+			if (currentMax==numbers[i]) {
 				index=i;
 				break;
 			}
 		}
 		return index;
 	}
-	
+
 	/**
 	 * Method "sum" receives an array and adds all the values 
-	 * @param sumArray receives an array
+	 * @param numbers receives an array
 	 * @return The sum of all the values 
 	 */
-	public static int sum(int[]sumArray) {
+	public static int sum(int[]numbers) {
 		int total=0;
-		for (int i=0;i<sumArray.length;i++) {
-			total+=sumArray[i];
+		for (int i=0;i<numbers.length;i++) {
+			total+=numbers[i];
 		}
-		return total;
-	}
-	
-	/**
-	 * Method "average" receives an array and gets the average all the values 
-	 * @param averageArray receives an array
-	 * @return The average of all the values 
-	 */
-	public static double average(int[]averageArray) {
-		double total=0;
-		for (int i=0;i<averageArray.length;i++) {
-			total+=averageArray[i];
-		}
-		total/=averageArray.length;
 		return total;
 	}
 
+	/**
+	 * Method "average" receives an array and gets the average of all the values 
+	 * @param numbers receives an array
+	 * @return The average of all the values 
+	 */
+	public static double average(int[]numbers) {
+		double total=sum(numbers);
+		total/=numbers.length;
+		return total;
+	}
+	
+	/**
+	 * 
+	 * @param numbers
+	 * @return
+	 */
+	public static int min(double [] numbers) {
+		double currentMin=numbers[0];
+		int index=0;
+		for (int i=0;i<numbers.length;i++) {
+			currentMin=Math.min(currentMin, numbers[i]);
+		}
+		for (int i=0;i<numbers.length;i++) {
+			if (currentMin==numbers[i]) {
+				index=i;
+				break;
+			}
+		}
+		return index;
+	}
+
+	/**
+	 * 
+	 * @param numbers
+	 * @return
+	 */
+	public static int max(double [] numbers) {
+		double currentMax=numbers[0];
+		int index=0;
+		for (int i=0;i<numbers.length;i++) {
+			currentMax=Math.max(currentMax, numbers[i]);
+		}
+
+		for (int i=0;i<numbers.length;i++) {
+			if (currentMax==numbers[i]) {
+				index=i;
+				break;
+			}
+		}
+		return index;
+	}
+	
+	/**
+	 * 
+	 * @param numbers
+	 * @return
+	 */
+	public static double sum(double [] numbers) {
+		int total=0;
+		for (int i=0;i<numbers.length;i++) {
+			total+=numbers[i];
+		}
+		return total;
+	}
+	/**
+	 * 
+	 * @param numbers
+	 * @return
+	 */
+	public static double average(double [] numbers) {
+		double total=sum(numbers);
+		total/=numbers.length;
+		return total;
+	}
 }
