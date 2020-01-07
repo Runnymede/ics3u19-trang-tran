a4b4144acc7ec9df0bb09cd7a10a92e224572ca8
333771657 <333771657@C-9R62YR1.tdsb.on.ca>
2020-01-07 13:41:01 -0500

    part 2

---
+++ ICS3U-Trang/src/extraChallenges/ColdCompress.java
@@ -0,0 +1,27 @@
+package extraChallenges;
+
+import java.util.Scanner;
+
+public class ColdCompress {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int lines=sc.nextInt();
+		String [] characters= new String [lines];
+		
+		for (int i=0;i<characters.length;i++) {
+			characters[i]=sc.next();
+		}
+		
+		for (int i=0;i<characters.length;i++) {
+		
+		}
+	}
+	
+	public static int count() {
+		
+		return 0;
+	}
+
+}
