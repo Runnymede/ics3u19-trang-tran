db4ecfc8eb5b995af43d753e663e3d5a9f5ab44a
333771657 <333771657@C-9R62YR1.tdsb.on.ca>
2020-01-13 13:29:16 -0500

    Today

--- ICS3U-Trang/src/tran/culminating/Mancala.java
+++ ICS3U-Trang/src/tran/culminating/Mancala.java
@@ -1,21 +1,108 @@
 package tran.culminating;
-import java.util.Scanner;
 import java.awt.*;
 import hsa_new.Console;
 
 /**
  * 
- * @author 333771657
- *
+ * Date: January 13, 2020
+ * @author Trang Tran
  */
 public class Mancala {
 	static Console c = new Console (320, 240, "Mancala");
+	static boolean playerTurn=true;
+
 	/**
 	 * Start of the program
 	 * @param args
 	 */
 	public static void main(String[] args)  throws InterruptedException{
+		int [] playerHoles= {4,4,4,4,4,4,0,4,4,4,4,4,4,0};
+		String playerAnswer;
+		int index=0;
 		
+		c.println("Mancala");
+		c.println("Do you want to play?");
+		playerAnswer=c.readLine();
+		
+		do {
+			while (gameOver(playerHoles)==false) {
+				if (playerTurn==true) {
+					drawScreen(playerHoles);
+					c.println("Which hole will you take the beads out of?");
+					index=c.readInt();
+					dropBeads(playerHoles, index);
+					
+					if (playAgain(playerHoles)==true) {
+						
+					}
+					else {
+
+					}
+				}
+				else {
+					drawScreen(playerHoles);
+					c.println("Which hole will you take the beads out of?");
+					index=c.readInt();
+					dropBeads(playerHoles, index);
+					
+					if (playAgain(playerHoles)==true) {
+
+					}
+					else {
+
+					}
+				}
+			}
+			
+			drawScreen (playerHoles);
+			c.println("Do you want to play again?");
+			playerAnswer=c.readLine();
+		}while(playerAnswer.equalsIgnoreCase("yes"));
 	}
 
+	/**
+	 * 
+	 * @param holes
+	 */
+	public static void drawScreen(int [] holes) {
+
+	}
+
+	/**
+	 * 
+	 * @param holes
+	 * @param index
+	 */
+	public static void dropBeads(int [] holes, int index) {
+
+	}
+
+	/**
+	 * 
+	 * @param index1
+	 * @param index2
+	 */
+	public static void capture(int index1, int index2) {
+
+	}
+
+	/**
+	 * 
+	 * @param holes
+	 * @return
+	 */
+	public static boolean gameOver(int [] holes) {
+
+		return false;
+	}
+
+	/**
+	 * 
+	 * @param holes
+	 * @return
+	 */
+	public static boolean playAgain(int [] holes) {
+
+		return true;
+	}
 }
