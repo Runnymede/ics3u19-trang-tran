b7c38a937876812cd21550dc9701c51bd7dcb488
333771657 <333771657@C-9R62YR1.tdsb.on.ca>
2020-01-13 13:02:51 -0500

    January 13, 2020

---
+++ ICS3U-Trang/src/tran/culminating/Mancala.java
@@ -0,0 +1,21 @@
+package tran.culminating;
+import java.util.Scanner;
+import java.awt.*;
+import hsa_new.Console;
+
+/**
+ * 
+ * @author 333771657
+ *
+ */
+public class Mancala {
+	static Console c = new Console (320, 240, "Mancala");
+	/**
+	 * Start of the program
+	 * @param args
+	 */
+	public static void main(String[] args)  throws InterruptedException{
+		
+	}
+
+}
