310ab2edf620376629b172ef32d56679739885db
Trang-TDSB <trang.tran2@student.tdsb.on.ca>
2020-01-19 20:10:04 -0500

    hi

--- ICS3U-Trang/src/extraChallenges/DivisibilityBy11.java
+++ ICS3U-Trang/src/extraChallenges/DivisibilityBy11.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class DivisibilityBy11 {
-
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in); 
 		 List<Integer> input = new ArrayList<>();
