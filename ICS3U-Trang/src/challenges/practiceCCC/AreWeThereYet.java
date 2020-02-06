f1edbf218de2bf02e4f4ea3dde2748103840e4a0
333771657 <333771657@C-9R62YR1.tdsb.on.ca>
2020-02-06 12:22:17 -0500

    part2

--- ICS3U-Trang/src/challenges/practiceCCC/AreWeThereYet.java
+++ ICS3U-Trang/src/challenges/practiceCCC/AreWeThereYet.java
@@ -5,6 +5,24 @@ public class AreWeThereYet {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
+		int [] output=new int [25];
+		
+		String []cities=sc.nextLine().split(" ");
+		int [] cityNum=new int [4];
+		
+		for (int i=0;i<cityNum.length;i++) {
+			cityNum[i]=Integer.parseInt(cities[i]);
+		}
+		
+		int counter=1;
+		while (counter<=5) {
+			distances(counter, cityNum);
+			
+		}
+	}
+	
+	public static void distances(int line, int [] input) {
+		
 	}
 	
 }
--- ICS3U-Trang/src/challenges/practiceCCC/Telemarketer.java
+++ ICS3U-Trang/src/challenges/practiceCCC/Telemarketer.java
@@ -1,5 +1,3 @@
-package challenges.practiceCCC;
-
 import java.util.Scanner;
 
 public class Main {
