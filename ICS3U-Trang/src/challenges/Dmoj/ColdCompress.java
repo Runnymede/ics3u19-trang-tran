bc79b7921bfebf14bfd0fe08b71a359d59223c24
333771657 <333771657@C-9R62YR1.tdsb.on.ca>
2020-02-11 12:21:52 -0500

    hey

--- ICS3U-Trang/src/challenges/Dmoj/ColdCompress.java
+++ ICS3U-Trang/src/challenges/Dmoj/ColdCompress.java
@@ -3,14 +3,44 @@ package challenges.Dmoj;
 import java.util.Scanner;
 
 public class ColdCompress {
-	
+
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		
+
+		int max=sc.nextInt();
+		String [] texts=new String[max];
+
+		for (int i=0; i<max; i++) {
+			texts[i]=sc.next();
+		}
+
+		for (int i=0; i<max; i++) {
+			count(texts[i]);
+		}
 	}
-	
+
 	public static void count(String textLine) {
-		
+
+		char current=textLine.charAt(0);
+		char next=textLine.charAt(1);
+		int counter=0;
+		int counter2=0;
+
+
+		for (int a=0;a<textLine.length();a++) {
+			if (current==a) {
+				counter++;
+			}
+			else {
+				counter2=a;
+			}
+		}
+		System.out.print(counter+" "+current+" ");
+		counter=0;
+		current=textLine.charAt(counter2);
+
+		System.out.println();
+
 	}
 
 }
--- ICS3U-Trang/src/challenges/Dmoj/TimeToDecompress.java
+++ ICS3U-Trang/src/challenges/Dmoj/TimeToDecompress.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main{
+public class TimeToDecompress{
 
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
